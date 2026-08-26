faa5ea2328f5d3d6902550a4493756aa09df68a4
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-26 22:55:34 +0300

    Add and implement the bubble sort mechanism in main

--- Files with Java/Sorting/src/Main.java
+++ Files with Java/Sorting/src/Main.java
@@ -13,7 +13,26 @@ public class Main {
         for(int i = 0 ; i < n ; i++)
             arr[i] = input.nextInt();
 
+        // Bubble sort
+        bubbleSort(arr,n);
+
+        System.out.println("After bubble sorting");
+        for(int i = 0 ; i < n ; i++)
+            System.out.print(arr[i] + " ");
 
 
+
+    }
+    static void bubbleSort(int arr[] , int n) {
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < n-1 ; j++) {
+                if(arr[j] > arr[j+1]) {
+                    // تبديل العناصر (Swap)
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 }
