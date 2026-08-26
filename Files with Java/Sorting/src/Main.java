3f014b137d69c3bf81f60b3f05a875ec410b218c
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-26 23:14:08 +0300

    Add and implement the selection sort mechanism in main

--- Files with Java/Sorting/src/Main.java
+++ Files with Java/Sorting/src/Main.java
@@ -14,9 +14,12 @@ public class Main {
             arr[i] = input.nextInt();
 
         // Bubble sort
-        bubbleSort(arr,n);
+        // bubbleSort(arr,n);
 
-        System.out.println("After bubble sorting");
+        // Selection sort\
+        selectionSort(arr,n);
+
+        System.out.println("After selection sorting");
         for(int i = 0 ; i < n ; i++)
             System.out.print(arr[i] + " ");
 
@@ -25,7 +28,7 @@ public class Main {
     }
     static void bubbleSort(int arr[] , int n) {
         for(int i = 0 ; i < n ; i++) {
-            for(int j = 0 ; j < n-1 ; j++) {
+            for(int j = 0 ; j < n-i-1 ; j++) {
                 if(arr[j] > arr[j+1]) {
                     // تبديل العناصر (Swap)
                     int temp = arr[j];
@@ -35,4 +38,22 @@ public class Main {
             }
         }
     }
+
+    static void selectionSort(int arr[] , int n) {
+        for(int i = 0 ; i < n-1 ; i++) {
+            int minIndex = i;
+            for(int j = i+1 ; j < n ; j++) {
+                if(arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            if(minIndex != i) {
+                // swap function
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+
 }
