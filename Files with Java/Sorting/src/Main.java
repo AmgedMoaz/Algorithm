485a5e4173d2be8e9dad58433bea068223c1154d
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-26 22:23:52 +0300

    Preparation the array

--- Files with Java/Sorting/src/Main.java
+++ Files with Java/Sorting/src/Main.java
@@ -3,8 +3,17 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the size of array : ");
+        int n = input.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++)
+            arr[i] = input.nextInt();
+
+
 
     }
 }
