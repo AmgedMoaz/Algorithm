f534a593ba65398c42eb246edfd0d21a8ad0fe95
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-26 22:01:05 +0300

    Add main class for implementing every kind of sort

---
+++ Files with Java/Sorting/src/Main.java
@@ -0,0 +1,10 @@
+// Write a main class for every kind of sort
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
