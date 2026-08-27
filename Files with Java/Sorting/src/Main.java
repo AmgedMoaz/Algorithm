a1c1b193f1856bdf3de466f9413c0aeac88b973e
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-27 16:57:08 +0300

    Add and fill the merge sort "Conquer section"

--- Files with Java/Sorting/src/Main.java
+++ Files with Java/Sorting/src/Main.java
@@ -84,5 +84,47 @@ public class Main {
             // merge(arr,left,mid,right);
         }
     }
+    static void merge(int arr[] , int left , int mid , int right) {
+
+        // 1. حساب أحجام المصفوفات المؤقتة
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // 2. إنشاء المصفوفات المؤقتة
+        int [] L = new int[n1];
+        int[]  R = new int [n2];
+
+        // 3. نسخ البيانات إلى المصفوفات المؤقتة
+        for(int i = 0 ; i < n1 ; i++)
+            L[i] = arr[left+i];
+        for(int i = 0 ; i < n2 ; i++)
+            R[i] = arr[mid+i+1];
+
+        // 4. دمج عناصر المصفوفات بالترتيب الصحيح
+        int i = 0 , j = 0 , k = left;
+
+        while(i < n1 && j < n2) {
+            if(L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // 5. نسخ العناصر المتبقية إن وجدت
+        while(i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while(j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
 
 }
