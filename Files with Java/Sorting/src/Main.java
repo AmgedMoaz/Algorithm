4fb347f7be6a81ab8fd1fb98c2485c4fcfae3253
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-27 16:49:48 +0300

    Add line to arrange the code

--- Files with Java/Sorting/src/Main.java
+++ Files with Java/Sorting/src/Main.java
@@ -27,6 +27,7 @@ public class Main {
             System.out.print(arr[i] + " ");
 
     }
+
     static void bubbleSort(int arr[] , int n) {
         for(int i = 0 ; i < n-1 ; i++) {
             for(int j = 0 ; j < n-i-1 ; j++) {
