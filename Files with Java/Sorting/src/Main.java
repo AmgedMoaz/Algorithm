28c2e66060cc5db72a9536276e0b878e0d9f7edb
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-28 23:53:56 +0300

    Add quick sort function

--- Files with Java/Sorting/src/Main.java
+++ Files with Java/Sorting/src/Main.java
@@ -129,4 +129,13 @@ public class Main {
         }
     }
 
+    static void quickSort(int arr[] , int low , int high) {
+        if(low < high) {
+            int pivotIndex = partition(arr,low,high);
+            quickSort(arr,low,pivotIndex-1);
+            quickSort(arr,pivotIndex+1,high);
+        }
+    }
+
+
 }
