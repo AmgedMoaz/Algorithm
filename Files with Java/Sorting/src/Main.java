b44803e417257fba66513dcac1833ce7f82bddea
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-29 19:02:44 +0300

    Add heap sort function "heap section"

--- Files with Java/Sorting/src/Main.java
+++ Files with Java/Sorting/src/Main.java
@@ -185,5 +185,26 @@ public class Main {
             heapify(arr,i,0);
         }
     }
+    static void heapify(int arr[],int n , int i) {
+        int largest = i;
+        int left = 2*i+1;
+        int right = 2*i+2;
+
+        if(left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        if(right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if(largest != i) {
+            // Swap function
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr,n,largest);
+        }
+    }
 
 }
