304b6375c248549c93682c4e490aead241728a87
Amged Moaz <amgedmoaz7@gmail.com>
2026-08-31 19:48:08 +0300

    Add binary search method

--- Files with Java/Searching/src/Main.java
+++ Files with Java/Searching/src/Main.java
@@ -1,6 +1,6 @@
 // Write a code to show meaning of searching algorithm
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main{
@@ -17,13 +17,26 @@ public class Main{
         System.out.print("Enter target you're looking for : ");
         int target = input.nextInt();
 
+        /*
         if(linearSearch(arr,n,target)) {
             System.out.println("Found");
         }else {
             System.out.println("Not found");
         }
+        */
+
+        // Like sort method in C++
+        Arrays.sort(arr);
+
+        int result = binarySearch(arr,n,target);
+        if(result <= -1) {
+            System.out.println("Not found");
+        }else {
+            System.out.println("Found");
+        }
 
     }
+
     // Linear search method
     static boolean linearSearch(int []arr , int n ,  int target) {
         for(int i = 0 ; i < n ; i++) {
@@ -32,5 +45,22 @@ public class Main{
             }
         }
         return false;
+    }
+
+    // Binary search method
+    static int binarySearch(int []arr , int n , int target) {
+        int left = 0;
+        int right = n-1;
+        while(left <= right) {
+            int mid = (left+right) / 2;
+            if(arr[mid] == target) {
+                return mid;
+            }else if(arr[mid] < target) {
+                left = mid+1;
+            }else {
+                right = mid-1;
+            }
+        }
+        return -1;
     }
 }
